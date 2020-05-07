cb0f6cb2418d30d0c161ebe7385fd7a38ccc759c
gabbersepp <gabbersepp@googlemail.com>
2020-05-07 22:33:40 +0200

    first changes to empty project

---
+++ blog-posts/net-internals/digging-into-callbacks/code/DevToNetProfiler/DevToNetProfiler/DevToNetProfiler_i.c
@@ -0,0 +1,84 @@
+
+
+/* this ALWAYS GENERATED file contains the IIDs and CLSIDs */
+
+/* link this file in with the server and any clients */
+
+
+ /* File created by MIDL compiler version 8.01.0622 */
+/* at Tue Jan 19 04:14:07 2038
+ */
+/* Compiler settings for DevToNetProfiler.idl:
+    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.01.0622 
+    protocol : dce , ms_ext, c_ext, robust
+    error checks: allocation ref bounds_check enum stub_data 
+    VC __declspec() decoration level: 
+         __declspec(uuid()), __declspec(selectany), __declspec(novtable)
+         DECLSPEC_UUID(), MIDL_INTERFACE()
+*/
+/* @@MIDL_FILE_HEADING(  ) */
+
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif 
+
+
+#include <rpc.h>
+#include <rpcndr.h>
+
+#ifdef _MIDL_USE_GUIDDEF_
+
+#ifndef INITGUID
+#define INITGUID
+#include <guiddef.h>
+#undef INITGUID
+#else
+#include <guiddef.h>
+#endif
+
+#define MIDL_DEFINE_GUID(type,name,l,w1,w2,b1,b2,b3,b4,b5,b6,b7,b8) \
+        DEFINE_GUID(name,l,w1,w2,b1,b2,b3,b4,b5,b6,b7,b8)
+
+#else // !_MIDL_USE_GUIDDEF_
+
+#ifndef __IID_DEFINED__
+#define __IID_DEFINED__
+
+typedef struct _IID
+{
+    unsigned long x;
+    unsigned short s1;
+    unsigned short s2;
+    unsigned char  c[8];
+} IID;
+
+#endif // __IID_DEFINED__
+
+#ifndef CLSID_DEFINED
+#define CLSID_DEFINED
+typedef IID CLSID;
+#endif // CLSID_DEFINED
+
+#define MIDL_DEFINE_GUID(type,name,l,w1,w2,b1,b2,b3,b4,b5,b6,b7,b8) \
+        EXTERN_C __declspec(selectany) const type name = {l,w1,w2,{b1,b2,b3,b4,b5,b6,b7,b8}}
+
+#endif // !_MIDL_USE_GUIDDEF_
+
+MIDL_DEFINE_GUID(IID, IID_INetProfiler,0x103d660d,0x1cb4,0x4410,0x85,0xdd,0x67,0xb3,0xaa,0x48,0x96,0x26);
+
+
+MIDL_DEFINE_GUID(IID, LIBID_DevToNetProfilerLib,0xe0145544,0x414d,0x487e,0x92,0x19,0x17,0x8b,0x87,0xff,0x0a,0xaa);
+
+
+MIDL_DEFINE_GUID(CLSID, CLSID_NetProfiler,0xb45048d5,0x6f44,0x4fbe,0xae,0x88,0xb4,0x68,0xa5,0xe4,0x92,0x7a);
+
+#undef MIDL_DEFINE_GUID
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
---
+++ blog-posts/net-internals/digging-into-callbacks/code/DevToNetProfiler/DevToNetProfiler/ProfilerCallback.cpp
@@ -0,0 +1,432 @@
+#include "pch.h"
+#include "ProfilerCallback.h"
+#include<iostream>
+#include <corprof.h>
+
+using namespace std;
+CComQIPtr<ICorProfilerInfo2> iCorProfilerInfo;
+
+ProfilerCallback::ProfilerCallback() {
+}
+
+HRESULT ProfilerCallback::FinalConstruct()
+{
+  return S_OK;
+}
+
+void ProfilerCallback::FinalRelease()
+{
+}
+
+HRESULT __stdcall ProfilerCallback::Initialize(IUnknown* pICorProfilerInfoUnk)
+{
+  pICorProfilerInfoUnk->QueryInterface(IID_ICorProfilerInfo2, (LPVOID*)&iCorProfilerInfo);
+  iCorProfilerInfo->SetEventMask(COR_PRF_MONITOR_EXCEPTIONS);
+
+  return S_OK;
+}
+
+void GetClassName(ObjectID objectId, char* output, ULONG outputLength) {
+  ClassID classId;
+  ModuleID moduleId;
+  mdTypeDef typeDefToken;
+  IMetaDataImport* metadata;
+  wchar_t* className = new wchar_t[100];
+  ULONG read = 0;
+
+  iCorProfilerInfo->GetClassFromObject(objectId, &classId);
+  iCorProfilerInfo->GetClassIDInfo(classId, &moduleId, &typeDefToken);
+  iCorProfilerInfo->GetModuleMetaData(moduleId, ofRead, IID_IMetaDataImport, (IUnknown**)&metadata);
+  metadata->GetTypeDefProps(typeDefToken, className, outputLength, &read, NULL, NULL);
+  metadata->Release();
+
+  memset(output, 0, 100);
+  wcstombs(output, className, 100);
+  delete[] className;
+}
+
+HRESULT __stdcall ProfilerCallback::ExceptionThrown(ObjectID thrownObjectID)
+{ // header: pragma disable warnign 
+  char* className = new char[100];
+  GetClassName(thrownObjectID, className, 100);
+  cout << "\t\nfrom profiler: exception thrown: " << className << "\r\n";
+  delete[] className;
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::Shutdown()
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::AppDomainCreationStarted(AppDomainID appDomainID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::AppDomainCreationFinished(AppDomainID appDomainID, HRESULT hrStatus)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::AppDomainShutdownStarted(AppDomainID appDomainID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::AppDomainShutdownFinished(AppDomainID appDomainID, HRESULT hrStatus)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::AssemblyLoadStarted(AssemblyID assemblyID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::AssemblyLoadFinished(AssemblyID assemblyID, HRESULT hrStatus)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::AssemblyUnloadStarted(AssemblyID assemblyID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::AssemblyUnloadFinished(AssemblyID assemblyID, HRESULT hrStatus)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ModuleLoadStarted(ModuleID moduleID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ModuleLoadFinished(ModuleID moduleID, HRESULT hrStatus)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ModuleUnloadStarted(ModuleID moduleID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ModuleUnloadFinished(ModuleID moduleID, HRESULT hrStatus)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ModuleAttachedToAssembly(ModuleID moduleID, AssemblyID assemblyID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ClassLoadStarted(ClassID classID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ClassLoadFinished(ClassID classID, HRESULT hrStatus)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ClassUnloadStarted(ClassID classID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ClassUnloadFinished(ClassID classID, HRESULT hrStatus)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::FunctionUnloadStarted(FunctionID functionID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::JITCompilationStarted(FunctionID functionID, BOOL fIsSafeToBlock)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::JITCompilationFinished(FunctionID functionID, HRESULT hrStatus, BOOL fIsSafeToBlock)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::JITCachedFunctionSearchStarted(FunctionID functionID, BOOL* pbUseCachedFunction)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::JITCachedFunctionSearchFinished(FunctionID functionID, COR_PRF_JIT_CACHE result)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::JITFunctionPitched(FunctionID functionID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::JITInlining(FunctionID callerID, FunctionID calleeID, BOOL* pfShouldInline)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::UnmanagedToManagedTransition(FunctionID functionID, COR_PRF_TRANSITION_REASON reason)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ManagedToUnmanagedTransition(FunctionID functionID, COR_PRF_TRANSITION_REASON reason)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ThreadCreated(ThreadID threadID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ThreadDestroyed(ThreadID threadID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ThreadAssignedToOSThread(ThreadID managedThreadID, DWORD osThreadID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::RemotingClientInvocationStarted()
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::RemotingClientSendingMessage(GUID* pCookie, BOOL fIsAsync)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::RemotingClientReceivingReply(GUID* pCookie, BOOL fIsAsync)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::RemotingClientInvocationFinished()
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::RemotingServerReceivingMessage(GUID* pCookie, BOOL fIsAsync)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::RemotingServerInvocationStarted()
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::RemotingServerInvocationReturned()
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::RemotingServerSendingReply(GUID* pCookie, BOOL fIsAsync)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::RuntimeSuspendStarted(COR_PRF_SUSPEND_REASON suspendReason)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::RuntimeSuspendFinished()
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::RuntimeSuspendAborted()
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::RuntimeResumeStarted()
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::RuntimeResumeFinished()
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::RuntimeThreadSuspended(ThreadID threadID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::RuntimeThreadResumed(ThreadID threadID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::MovedReferences(ULONG cmovedObjectIDRanges, ObjectID oldObjectIDRangeStart[], ObjectID newObjectIDRangeStart[], ULONG cObjectIDRangeLength[])
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ObjectAllocated(ObjectID objectID, ClassID classID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ObjectsAllocatedByClass(ULONG classCount, ClassID classIDs[], ULONG objects[])
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ObjectReferences(ObjectID objectID, ClassID classID, ULONG objectRefs, ObjectID objectRefIDs[])
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::RootReferences(ULONG rootRefs, ObjectID rootRefIDs[])
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ExceptionUnwindFunctionEnter(FunctionID functionID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ExceptionUnwindFunctionLeave()
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ExceptionSearchFunctionEnter(FunctionID functionID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ExceptionSearchFunctionLeave()
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ExceptionSearchFilterEnter(FunctionID functionID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ExceptionSearchFilterLeave()
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ExceptionSearchCatcherFound(FunctionID functionID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ExceptionCLRCatcherFound()
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ExceptionCLRCatcherExecute()
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ExceptionOSHandlerEnter(FunctionID functionID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ExceptionOSHandlerLeave(FunctionID functionID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ExceptionUnwindFinallyEnter(FunctionID functionID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ExceptionUnwindFinallyLeave()
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ExceptionCatcherEnter(FunctionID functionID,
+  ObjectID objectID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ExceptionCatcherLeave()
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::COMClassicVTableCreated(ClassID wrappedClassID, REFGUID implementedIID, void* pVTable, ULONG cSlots)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::COMClassicVTableDestroyed(ClassID wrappedClassID, REFGUID implementedIID, void* pVTable)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::ThreadNameChanged(ThreadID threadID, ULONG cchName, WCHAR name[])
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::GarbageCollectionStarted(int cGenerations, BOOL generationCollected[], COR_PRF_GC_REASON reason)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::SurvivingReferences(ULONG cSurvivingObjectIDRanges, ObjectID objectIDRangeStart[], ULONG cObjectIDRangeLength[])
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::GarbageCollectionFinished()
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::FinalizeableObjectQueued(DWORD finalizerFlags, ObjectID objectID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::RootReferences2(ULONG cRootRefs, ObjectID rootRefIDs[], COR_PRF_GC_ROOT_KIND rootKinds[], COR_PRF_GC_ROOT_FLAGS rootFlags[], UINT_PTR rootIDs[])
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::HandleCreated(GCHandleID handleID, ObjectID initialObjectID)
+{
+  return S_OK;
+}
+
+HRESULT __stdcall ProfilerCallback::HandleDestroyed(GCHandleID handleID)
+{
+  return S_OK;
+}
+
