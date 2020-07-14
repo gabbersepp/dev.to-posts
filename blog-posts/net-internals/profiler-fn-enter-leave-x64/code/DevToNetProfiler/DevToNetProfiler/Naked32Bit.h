c1da135e970273d651353942da107ff86530e175
gabbersepp <gabbersepp@googlemail.com>
2020-07-15 00:00:07 +0200

    Revert "test fastcall implementation of clr"
    This reverts commit 41a42afb151501f9d7abd313ed9828ca79322edd.

--- blog-posts/net-internals/profiler-fn-enter-leave-x64/code/DevToNetProfiler/DevToNetProfiler/Naked32Bit.h
+++ blog-posts/net-internals/profiler-fn-enter-leave-x64/code/DevToNetProfiler/DevToNetProfiler/Naked32Bit.h
@@ -1,6 +1,26 @@
 #pragma once
 #include "pch.h"
 
+#ifdef _WIN64
+
+EXTERN_C void InitEnterLeaveCallbacks(bool* activate, int* hashMap, int size);
+
+EXTERN_C void FnEnterCallback(FunctionID funcId,
+  UINT_PTR clientData,
+  COR_PRF_FRAME_INFO func,
+  COR_PRF_FUNCTION_ARGUMENT_INFO* argumentInfo);
+
+EXTERN_C void FnLeaveCallback(FunctionID funcId,
+  UINT_PTR clientData,
+  COR_PRF_FRAME_INFO func,
+  COR_PRF_FUNCTION_ARGUMENT_INFO* argumentInfo);
+
+EXTERN_C void FnTailcallCallback(FunctionID funcId,
+  UINT_PTR clientData,
+  COR_PRF_FRAME_INFO func);
+
+#else
+
 void InitEnterLeaveCallbacks(bool* activate, int* hashMap, int size);
 
 void FnEnterCallback(FunctionID funcId,
@@ -17,4 +37,4 @@ void FnTailcallCallback(FunctionID funcId,
   UINT_PTR clientData,
   COR_PRF_FRAME_INFO func);
 
-
+#endif
